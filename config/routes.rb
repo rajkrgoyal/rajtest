5043a3585f398435d1e991cca80945433d3e9d44
Raj <goyalkrraj@gmail.com>
2012-01-06 22:01:24 +0530

    added contacts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rajtest::Application.routes.draw do
+  resources :contacts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Rajtest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'contacts#index'
 
   # See how all your routes lay out with "rake routes"
 
